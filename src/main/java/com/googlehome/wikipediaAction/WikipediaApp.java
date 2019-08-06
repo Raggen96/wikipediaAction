b27cf55a8a298a76b3cac10c88fcc9e2c8fbfb10
Raggen96 <31734956+Raggen96@users.noreply.github.com>
2019-08-06 13:04:57 +0200

    endringer i wikiappen

--- src/main/java/com/googlehome/wikipediaAction/WikipediaApp.java
+++ src/main/java/com/googlehome/wikipediaAction/WikipediaApp.java
@@ -1,4 +1,31 @@
 package com.googlehome.wikipediaAction;
 
-public class WikipediaApp{
+
+import com.google.actions.api.ActionRequest;
+import com.google.actions.api.ActionResponse;
+import com.google.actions.api.DialogflowApp;
+import com.google.actions.api.ForIntent;
+import com.google.actions.api.response.ResponseBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WikipediaApp extends DialogflowApp {
+
+
+    private final Logger LOGGER = (Logger) LoggerFactory.getLogger(WikipediaApp.class);
+    @ForIntent("PersonIntent")
+    public ActionResponse personIntent(ActionRequest request) {
+        LOGGER.info("Welcome to Person Intent");
+        ResponseBuilder responseBuilder = getResponseBuilder(request);
+        String[] wikiTekst = wiki_test.getRandomArticle();
+        responseBuilder.add(wikiTekst[1]);
+        LOGGER.info("Person intent end.");
+        return responseBuilder.build();
+    }
+
+
+
+
 }
